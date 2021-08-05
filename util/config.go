06be2008bb6710c685772ba33762a089222d0b20
liuweiyin-hg <liuwyok@sina.com>
2021-08-05 17:00:39 +0800

    first commit

---
+++ util/config.go
@@ -0,0 +1,33 @@
+package util
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBUsername string `mapstructure:"DB_USER"`
+	DBPassword string `mapstructure:"DB_PASSWORD"`
+	DBHost     string `mapstructure:"DB_HOST"`
+	DBPort     string `mapstructure:"DB_PORT"`
+	DBName     string `mapstructure:"DB_NAME"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
+
+func EnvGet(varName string) interface{} {
+	return viper.Get(varName)
+}
